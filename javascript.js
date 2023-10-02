2d19f4cf75893322e0bd231bcabe111b0c6df657
Sam Rogers <sam.rogers.kiwi@gmail.com>
2023-10-02 18:48:14 +1300

    added min password length and fixed validiation issue

--- javascript.js
+++ javascript.js
@@ -7,10 +7,18 @@ let password2 = document.getElementById('confirmPwd');
 let subButton = document.getElementById('subButton');
 
 subButton.addEventListener('click', () => {
+    if((password2.value).length >= 6){
+        password2.setCustomValidity('');
     if(password1.value != password2.value){
         password2.setCustomValidity('Passwords do not match');
         password1.style.borderColor = "red";
         password2.style.borderColor = "red";
-        
+    }
+    else {
+        password2.setCustomValidity('');
+    }
+}
+    else {
+        password2.setCustomValidity('Password must be atleast 6 characters')
     }
 })
